7699037164dc907f7b1c4c35e2165929876a6646
janetbradley0927 <janet0927b@outlook.com>
2025-10-04 15:05:10 +1000

    delete unused code

--- src/types/card_type.rs
+++ src/types/card_type.rs
@@ -12,65 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rusqlite::ToSql;
-use rusqlite::types::ToSqlOutput;
-
-use crate::error::ErrorReport;
-use crate::error::fail;
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum CardType {
     Basic,
     Cloze,
-}
-
-impl CardType {
-    fn as_str(&self) -> &str {
-        match self {
-            CardType::Basic => "basic",
-            CardType::Cloze => "cloze",
-        }
-    }
-}
-
-impl TryFrom<String> for CardType {
-    type Error = ErrorReport;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        match value.as_str() {
-            "basic" => Ok(CardType::Basic),
-            "cloze" => Ok(CardType::Cloze),
-            _ => fail(format!("Invalid card type: '{}'", value)),
-        }
-    }
-}
-
-impl ToSql for CardType {
-    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
-        Ok(ToSqlOutput::from(self.as_str()))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::error::Fallible;
-
-    #[test]
-    fn test_card_type_serialization_roundtrip() -> Fallible<()> {
-        let card_types = [CardType::Basic, CardType::Cloze];
-        for ct in card_types {
-            assert_eq!(ct, CardType::try_from(ct.as_str().to_string())?);
-        }
-        Ok(())
-    }
-
-    #[test]
-    fn test_card_type_invalid() {
-        let invalid = "foo".to_string();
-        let res = CardType::try_from(invalid);
-        assert!(res.is_err());
-        let err = res.err().unwrap();
-        assert_eq!(err.to_string(), "error: Invalid card type: 'foo'");
-    }
 }
