766c9463ad56c5b1b06e1d0118b7ad263d65cb07
la Fleur <35574148+lafleurdeboum@users.noreply.github.com>
2025-10-30 01:14:27 +0100

    Add a nix flake - no build fixes (#41)

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        craneLib = crane.mkLib pkgs;
+        # Crane filters source files, so that mods to eg README.md don't
+        # trigger a rebuild. srcFilter adds css, js and sql files to
+        # the filtered source files.
+        srcFilter = path: _type: builtins.match ".*css$|.*js$|.*sql$" path != null;
+        allSrcFilter = path: type:
+          (srcFilter path type) || (craneLib.filterCargoSources path type);
+
+      in
+      {
+        packages.default = craneLib.buildPackage {
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = allSrcFilter;
+            # Be reproducible, regardless of the directory name :
+            name = "source";
+          };
+          # Do not run cargo tests on build, because cargo takes so
+          # long anyway :
+          doCheck = false;
+          buildInputs = with pkgs; [
+            sqlite
+            openssl
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+        };
+      });
+}
