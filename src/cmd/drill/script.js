7d7d9687da284d9c845bebd63e1d4c6e0cb6f868
janetbradley0927 <janet0927b@outlook.com>
2025-11-18 14:47:10 +1100

    Fix KaTeX initial rendering flicker [fix #58]

--- src/cmd/drill/script.js
+++ src/cmd/drill/script.js
@@ -22,6 +22,10 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
     macros: MACROS,
   });
+  const cardContent = document.querySelector(".card-content");
+  if (cardContent) {
+    cardContent.style.opacity = "1";
+  }
 });
 
 document.addEventListener("keydown", function (event) {
